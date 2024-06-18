ca3120fa71f4c7d9568c23572831e3bd4cda2f22
Sai Roopesh <sairoopesh21@gmail.com>
2024-06-18 20:52:28 +0530

    commit files

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,10 @@
-import adapter from '@sveltejs/adapter-cloudflare';
-
-export default {
+import cloudflare from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	kit: {
-		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			},
-			platformProxy: {
-				configPath: 'wrangler.toml',
-				environment: undefined,
-				experimentalJsonConfig: false,
-				persist: false
-			}
-		})
-	}
+		adapter: cloudflare()
+	},
+	preprocess: vitePreprocess()
 };
+export default config;
